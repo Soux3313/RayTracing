1870e5039874e9a098a28d428178009086e1fa68
Franz <fraja2004@yahoo.coom>
2023-10-19 08:25:00 +0200

    finished vector tests

--- src/test/java/org/example/VectorTest.java
+++ src/test/java/org/example/VectorTest.java
@@ -6,17 +6,44 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class VectorTest {
 
-
+/*Scenario: adding two vectors
+Given vector1 is a Vector(3.0, -2.0, 5.0)
+And vector2 is a Vector(-2.0, 3.0, 1.0)
+Then vector1 + vector2 = Vector(1.0, 1.0, 6.0)*/
     @Test
     void add() {
+        Vector v1 = new Vector(3,-2,5);
+        Vector v2 = new Vector(-2,3,1);
+        Vector add = v1.add(v2);
+
+        double[] expected = {1,1,6};
+        double[] actual = {add.getX(), add.getY(), add.getZ()};
+
+        assertArrayEquals(expected,actual);
     }
 
     @Test
     void subtract() {
+        Vector v1 = new Vector(3,2,1);
+        Vector v2 = new Vector(5,6,7);
+        Vector minusV = v1.subtract(v2);
+
+        double[] expected = {-2, -4, -6};
+        double[] actual = {minusV.getX(), minusV.getY(), minusV.getZ()};
+
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void subtractZeroVector(){
+    Vector v1 = new Vector(0,0,0);
+    Vector v2 = new Vector(1,-2,3);
+    Vector minusV = v1.subtract(v2);
+
+    double[] expected = {-1, 2, -3};
+    double[] actual = {minusV.getX(), minusV.getY(), minusV.getZ()};
+
+    assertArrayEquals(expected, actual);
 
     }
 
@@ -66,21 +93,82 @@ class VectorTest {
 
     @Test
     void magnitude() {
+        Vector v = new Vector(1,2,3);
+        double actualMag = v.magnitude();
+        double expectedMag = 3.7416573867739413;
+
+        assertEquals(actualMag, expectedMag);
     }
+
 
     @Test
     void sqrMagnitude() {
+        Vector v = new Vector(1,2,3);
+        double actualMag = v.sqrMagnitude();
+        double expectedMag = 14.0;
+
+        assertEquals(expectedMag, actualMag);
     }
 
     @Test
     void normalized() {
-    }
+        Vector v = new Vector(4,0,0);
+        Vector n = v.normalized();
 
+        double[] expected = {1,0,0};
+        double[] actual = {n.getX(), n.getY(), n.getZ()};
+
+        assertArrayEquals(expected, actual);
+    }
+/*Scenario: The magnitude of a normalized vector
+Given vector is a Vector(1, 2, 3)
+When vector is normalize(vector)
+Then magnitude(vector) = 1
+
+ */
+    @Test
+    void normalizedMagnitude()
+    {
+        Vector v = new Vector(1,2,3);
+        Vector n = v.normalized();
+        double mag = n.magnitude();
+
+        assertEquals(1,mag);
+    }
+    /*Scenario: The dot product of two vectors
+Given vector1 is a Vector(1, 2, 3)
+And vector2 is a Vector(2, 3, 4)
+Then dot(vector1, vector2) = 20
+*/
     @Test
     void dot() {
+        Vector v1 = new Vector(1,2,3);
+        Vector v2 = new Vector(2,3,4);
+        double dot = v1.dot(v2);
+
+        assertEquals(20,dot);
     }
 
+    /*Scenario: The cross product of two vectors
+Given vector1 is a Vector(1, 2, 3)
+And vector2 is a Vector(2, 3, 4)
+Then cross(vector1, vector2) = Vector(-1, 2, -1)
+And cross(vector2, vector1) = Vector(1, -2, 1)
+*/
     @Test
     void cross() {
+        Vector v1 = new Vector(1,2,3);
+        Vector v2 = new Vector(2,3,4);
+        Vector cross1 = v1.cross(v2);
+        Vector cross2 = v2.cross(v1);
+
+        double[] expected1 = {-1,2,-1};
+        double[] expected2 = {1,-2,1};
+
+        double[] actual1 = {cross1.getX(), cross1.getY(), cross1.getZ()};
+        double[] actual2 = {cross2.getX(), cross2.getY(), cross2.getZ()};
+
+        assertArrayEquals(expected1, actual1);
+        assertArrayEquals(expected2, actual2);
     }
 }
